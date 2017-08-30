d7f6b9add3b06c74aab6c2ddbfc87e48de9eec41
zhangzhi0903 <18310127735@163.com>
2017-08-30 10:50:17 +0800

    Initial Commit

---
+++ ZG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZG
+//
+//  Created by offcn_zcz32036 on 2017/8/30.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
