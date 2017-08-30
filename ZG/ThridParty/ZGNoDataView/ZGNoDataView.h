dba6c1e91ae72aa3fc4023336c948f439b395958
zhangzhi0903 <18310127735@163.com>
2017-08-30 13:34:49 +0800

    无网络页面加载

---
+++ ZG/ThridParty/ZGNoDataView/ZGNoDataView.h
@@ -0,0 +1,13 @@
+//
+//  ZGNoDataView.h
+//  ZG
+//
+//  Created by offcn_zcz32036 on 2017/8/30.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZGNoDataView : UIView
+
+@end
---
+++ ZG/ThridParty/ZGNoDataView/ZGNoDataView.m
@@ -0,0 +1,28 @@
+//
+//  ZGNoDataView.m
+//  ZG
+//
+//  Created by offcn_zcz32036 on 2017/8/30.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import "ZGNoDataView.h"
+
+@implementation ZGNoDataView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self createUI];
+    }
+    return self;
+}
+#pragma mark - createViews
+-(void)createUI
+{
+    
+}
+
+
+@end
